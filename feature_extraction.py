f9cdd5a6e2a97089b6e5918bd011698fdb7e6be9
liqichen <liqichen@me.com>
2019-07-01 16:37:07 +0800

    'more features'

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from python_speech_features import mfcc
+import librosa
 
 data_path = 'data_backup/1minbar'
 frame_length = 40
@@ -41,7 +42,7 @@ def get_dataframe() -> dict:
 
     return data_dict
 
-def get_mfcc_feature(all_data, past_day_num=5, fre=16000):
+def get_feature(all_data, past_day_num=5, fre=16000):
     company_list = get_company_name()
     for company_name in company_list:
         folder = os.path.exists(store_path + '/' + 'x_train')
@@ -50,22 +51,17 @@ def get_mfcc_feature(all_data, past_day_num=5, fre=16000):
 
 
         close_price = all_data[company_name][0].iloc[:,-2].values
-        volume = all_data[company_name][0].iloc[:,-1].values
+        volume = all_data[company_name][0].iloc[:,-1].values.astype(np.float)
 
-        mfcc_price = mfcc(close_price - np.mean(close_price), fre, close_price.shape[0]/fre, close_price.shape[0]/fre, nfft = close_price.shape[0],numcep=14)
-        mfcc_volume = mfcc(volume - np.mean(volume), fre, volume.shape[0]/fre, volume.shape[0]/fre, nfft = volume.shape[0],numcep=14)
-        feature = np.append(mfcc_price, mfcc_volume, axis=0)
+        feature = get_all_feature(close_price, volume)
 
         feature = feature[np.newaxis, :]
         num_days = len(all_data[company_name])
         for i in range(1, num_days):
             close_price = all_data[company_name][0].iloc[:,-2].values
-            volume = all_data[company_name][0].iloc[:,-1].values
-            mfcc_price = mfcc(close_price - np.mean(close_price), fre, close_price.shape[0]/fre, close_price.shape[0]/fre, nfft = close_price.shape[0],numcep=14)
-            mfcc_volume = mfcc(volume - np.mean(volume), fre, volume.shape[0]/fre, volume.shape[0]/fre, nfft = volume.shape[0],numcep=14)
-            new_feature = np.append(mfcc_price, mfcc_volume, axis=0)
+            volume = all_data[company_name][0].iloc[:,-1].values.astype(np.float)
+            new_feature = get_all_feature(close_price, volume)
             new_feature = new_feature[np.newaxis,:]
-
             feature = np.append(feature, new_feature, axis=0)
 
         recur_feature = feature[np.newaxis, 0:past_day_num, :]
@@ -73,8 +69,8 @@ def get_mfcc_feature(all_data, past_day_num=5, fre=16000):
             recur_feature = np.append(recur_feature, feature[np.newaxis, i:i+past_day_num,:], axis = 0)
 
         num_days = num_days - past_day_num
-        np.save(store_path + '/' + 'x_train' + '/' + company_name +'_feature_train', recur_feature[: int(0.8*num_days)])
-        np.save(store_path + '/' + 'x_test' + '/' + company_name + '_feature_test',
+        np.save(store_path + '/' + 'x_feature_train' + '/' + company_name +'_feature_train', recur_feature[: int(0.8*num_days)])
+        np.save(store_path + '/' + 'x_feature_test' + '/' + company_name + '_feature_test',
                 recur_feature[int(0.8 * num_days):])
         get_prediction(all_data, past_day_num)
 
@@ -112,10 +108,141 @@ def get_prediction(all_data, past_day_num=5):
         np.save(store_path + '/' + 'y_train' + '/' + company_name +'_y_train', save[: int(0.8*num_days)])
         np.save(store_path + '/' + 'y_test' + '/' + company_name + '_y_test', save[int(0.8 * num_days):])
 
+def get_all_feature(close_price: np.ndarray, volume:np.ndarray):
+    price_feature = librosa.feature.mfcc(close_price)
+    volume_feature = librosa.feature.mfcc(volume)
+
+    zrate_price = librosa.feature.zero_crossing_rate(close_price - np.mean(close_price))
+    zrate_volume = librosa.feature.zero_crossing_rate(volume - np.mean(volume))
+    price_feature = np.append(price_feature, zrate_price, axis=0)
+    volume_feature = np.append(volume_feature, zrate_volume, axis=0)
+
+    chromagram_price = librosa.feature.chroma_stft(close_price, n_chroma=12)
+    chromagram_volume = librosa.feature.chroma_stft(volume, n_chroma=12)
+    price_feature = np.append(price_feature, chromagram_price, axis=0)
+    volume_feature = np.append(volume_feature, chromagram_volume, axis=0)
+
+    cqt_price = librosa.feature.chroma_cqt(close_price, n_chroma=12)
+    cqt_volume = librosa.feature.chroma_cqt(volume, n_chroma=12)
+    price_feature = np.append(price_feature, cqt_price, axis=0)
+    volume_feature = np.append(volume_feature, cqt_volume, axis=0)
+
+    cens_price = librosa.feature.chroma_cens(close_price, n_chroma=12)
+    cens_volume = librosa.feature.chroma_cens(volume, n_chroma=12)
+    price_feature = np.append(price_feature, cens_price, axis=0)
+    volume_feature = np.append(volume_feature, cens_volume, axis=0)
+
+    mel_price = librosa.feature.melspectrogram(close_price - np.mean(close_price), n_mels=12)
+    mel_volume = librosa.feature.melspectrogram(volume - np.mean(volume), n_mels=12)
+    price_feature = np.append(price_feature, mel_price, axis=0)
+    volume_feature = np.append(volume_feature, mel_volume, axis=0)
+
+    rms_price = librosa.feature.rms(close_price)
+    rms_volume = librosa.feature.rms(volume)
+    price_feature = np.append(price_feature, rms_price, axis=0)
+    volume_feature = np.append(volume_feature, rms_volume, axis=0)
+
+    sc_price = librosa.feature.spectral_centroid(close_price)
+    sc_volume = librosa.feature.spectral_centroid(volume)
+    price_feature = np.append(price_feature, sc_price, axis=0)
+    volume_feature = np.append(volume_feature, sc_volume, axis=0)
+
+    bw1_price = librosa.feature.spectral_bandwidth(close_price, p=1)
+    bw1_volume = librosa.feature.spectral_bandwidth(volume, p=1)
+    price_feature = np.append(price_feature, bw1_price, axis=0)
+    volume_feature = np.append(volume_feature, bw1_volume, axis=0)
+
+    scon_price = librosa.feature.spectral_contrast(close_price)
+    scon_volume = librosa.feature.spectral_contrast(volume)
+    price_feature = np.append(price_feature, scon_price, axis=0)
+    volume_feature = np.append(volume_feature, scon_volume, axis=0)
+
+    sf_price = librosa.feature.spectral_flatness(close_price)
+    sf_volume = librosa.feature.spectral_flatness(volume)
+    price_feature = np.append(price_feature, sf_price, axis=0)
+    volume_feature = np.append(volume_feature, sf_volume, axis=0)
+
+    ro_price = librosa.feature.spectral_rolloff(close_price - np.mean(close_price))
+    ro_volume = librosa.feature.spectral_rolloff(volume - np.mean(volume))
+    price_feature = np.append(price_feature, ro_price, axis=0)
+    volume_feature = np.append(volume_feature, ro_volume, axis=0)
+
+    pf1_price = librosa.feature.poly_features(close_price - np.mean(close_price), order=2)
+    pf1_volume = librosa.feature.poly_features(volume - np.mean(volume), order=2)
+    price_feature = np.append(price_feature, pf1_price, axis=0)
+    volume_feature = np.append(volume_feature, pf1_volume, axis=0)
+
+    ton_price = librosa.feature.tonnetz(close_price)
+    ton_volume = librosa.feature.tonnetz(volume)
+    price_feature = np.append(price_feature, ton_price, axis=0)
+    volume_feature = np.append(volume_feature, ton_volume, axis=0)
+
+    price_feature = price_feature.T
+    volume_feature = volume_feature.T
+    feature = np.append(price_feature, volume_feature, axis=0)
+
+    return feature
+
+def get_market_feature(all_data: pd.DataFrame, past_day_num =5):
+    company_list = get_company_name()
+    for company_name in company_list:
+
+        num_days = len(all_data[company_name])
+        close_price = []
+        all_close_price = []
+        all_return = []
+        for i in range(num_days):
+            close_price.append(all_data[company_name][i].iloc[-1, -2])
+            close = all_data[company_name][i].iloc[:, -2].values
+            high = all_data[company_name][i].iloc[:, -4].values
+            low = all_data[company_name][i].iloc[:, -3].values
+            all_return.append((high-low)/close)
+            all_close_price.append(close)
+
+
+        company_returns = 0
+        market_feature = []
+        company_spr = (all_close_price[0].max() - all_close_price[0].min())/close_price[0]
+        r2 = (all_return[0] * all_return[0]).sum() / all_return[0].shape[0]
+        r2_down = (all_return[0][all_return[0]>0] ** 2).sum() / all_return[0].shape[0]
+        s = company_returns/(r2)**(1/2)
+        s_down = company_returns/(r2_down)**(1/2)
+        market_feature = np.array([company_returns, company_spr, r2, r2_down, s, s_down])
+        market_feature = market_feature[np.newaxis, :]
+
+
+        for i in range(1, num_days):
+            company_returns= (close_price[i]-close_price[i-1])/close_price[i-1]
+            company_spr = (all_close_price[i].max() - all_close_price[i].min())/close_price[i]
+            r2 = (all_return[i] * all_return[i]).sum() / all_return[i].shape[0]
+            r2_down = (all_return[i][all_return[i] > 0] ** 2).sum() / all_return[i].shape[0]
+            s = company_returns/(r2)**(1/2)
+            s_down = company_returns/(r2_down)**(1/2)
+            new_feature = np.array([company_returns, company_spr, r2, r2_down, s, s_down])
+            new_feature = new_feature[np.newaxis, :]
+            market_feature = np.append(market_feature, new_feature, axis=0)
+
+        recur_feature = market_feature[np.newaxis, 0:past_day_num, :]
+        for i in range(1, num_days - past_day_num):
+            recur_feature = np.append(recur_feature, market_feature[np.newaxis, i:i + past_day_num, :], axis=0)
+
+        num_days = num_days - past_day_num
+        np.save(store_path + '/' + 'x_market_train' + '/' + company_name + '_market_train',
+                recur_feature[: int(0.8 * num_days)])
+        np.save(store_path + '/' + 'x_market_test' + '/' + company_name + '_market_test',
+                recur_feature[int(0.8 * num_days):])
+
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
     all_data = get_dataframe()
-    get_mfcc_feature(all_data)
+    get_market_feature(all_data)
+    get_feature(all_data)
     get_prediction(all_data)
--- han_training.py
+++ han_training.py
@@ -5,6 +5,7 @@ from keras import backend as K
 import keras
 import os
 import numpy as np
+import tensorflow as tf
 from keras.utils.np_utils import to_categorical
 
 
@@ -12,12 +13,16 @@ from keras.utils.np_utils import to_categorical
 
 def han():
     # refer to 4.2 in the paper whil reading the following code
+    #window_size = 5
+    market_input = Input(shape=(5, 6), dtype='float32' )
+    market_out = Dense(90, activation='tanh')(market_input)
+    market_out1 = Lambda(lambda x: K.expand_dims(x, axis=2))(market_out)
 
     # Input for one day : max article per day =40, dim_vec=200
-    input1 = Input(shape=(2, 14), dtype='float32')
+    input1 = Input(shape=(3, 90), dtype='float32')
 
     # Attention Layer
-    dense_layer = Dense(14, activation='tanh')(input1)
+    dense_layer = Dense(90, activation='tanh')(input1)
     softmax_layer = Activation('softmax')(dense_layer)
     attention_mul = multiply([softmax_layer,input1])
     #end attention layer
@@ -28,25 +33,26 @@ def han():
     pre_model2 = Model(input1, vec_sum)
 
     # Input of the HAN shape (None,11,40,200)
-    # 11 = Window size = N in the paper 40 = max articles per day, dim_vec = 200
-    input2 = Input(shape=(5, 2, 14), dtype='float32')
+    # 5 = Window size = N in the paper 40 = max articles per day, dim_vec = 200
+    input2 = Input(shape=(5, 2, 90), dtype='float32')
 
     # TimeDistributed is used to apply a layer to every temporal slice of an input
     # So we use it here to apply our attention layer ( pre_model ) to every article in one day
     # to focus on the most critical article
-    pre_gru = TimeDistributed(pre_model1)(input2)
+    combine = keras.layers.concatenate([input2, market_out1], axis=2)
+    pre_gru = TimeDistributed(pre_model1)(combine)
 
 # bidirectional gru
-    l_gru = Bidirectional(GRU(7, return_sequences=True))(pre_gru)
+    l_gru = Bidirectional(GRU(45, return_sequences=True))(pre_gru)
 
     # We apply attention layer to every day to focus on the most critical day
     post_gru = TimeDistributed(pre_model2)(l_gru)
 
 # MLP to perform classification
-    dense1 = Dense(20, activation='tanh')(post_gru)
+    dense1 = Dense(90, activation='tanh')(post_gru)
     dense2 = Dense(3, activation='tanh')(dense1)
     final = Activation('softmax')(dense2)
-    final_model = Model(input2, final)
+    final_model = Model(inputs=[input2, market_input], outputs=[final])
     final_model.summary()
 
     return final_model
@@ -101,29 +107,37 @@ def load_data(model, x_train_file, x_test_file, y_train_file, y_test_file):
 
 
 
-def twin_creation( x_train_folder, y_train_folder) :
+def twin_creation(x_feature_folder, x_market_folder, y_train_folder) :
     ''' Here we create a list of twins ( duo_list)
 	Twin = [CompanyA_x_train_filepath, CompanyA_y_train_filepath]'''
-    x_train_list= os.listdir(x_train_folder)
-    x_train_list=sorted(x_train_list)
+    x_feature_list= os.listdir(x_feature_folder)
+    x_feature_list=sorted(x_feature_list)
+    if x_feature_list[0] == '.DS_Store':
+        x_feature_list.pop(0)
+
+    x_market_folder = os.listdir(x_market_folder)
+    x_market_list = sorted(x_market_folder)
 
 
     y_train_list=os.listdir(y_train_folder)
     y_train_list=sorted(y_train_list)
+    if y_train_list[0] == '.DS_Store':
+        y_train_list.pop(0)
 
 
     duo_list=[]
     for i in range (len(y_train_list)):
-        duo=[x_train_list[i],y_train_list[i]]
+        duo=[x_feature_list[i], x_market_list[i], y_train_list[i]]
         duo_list.append(duo)
 
     duo_list=[duo for duo in duo_list if duo[0][-4:]=='.npy']
 
-	
     return duo_list
-def training(x_name,y_name,model):
-    x_train = np.load('data_backup/feed_data/x_train/'+x_name)
+
+def training(x_feature_name,x_market_name,y_name,model):
+    x_feature = np.load('data_backup/feed_data/x_feature_train/'+x_feature_name)
     y_train = np.load('data_backup/feed_data/y_train/'+y_name)
+    x_market = np.load('data_backup/feed_data/x_market_train/'+x_market_name)
 
 
     y_oh_list=[] # y one hot for one hot encoding
@@ -141,31 +155,40 @@ def training(x_name,y_name,model):
     #encoder = LabelEncoder()
     #encoder.fit(y_train)
     #encoded_Y = encoder.transform(y_train)
-    print("model fitting on " + x_name)
+    print("model fitting on " + x_feature_name)
     for i in range(10):
-        train = model.train_on_batch(x_train, y_train_end)
+        train = model.train_on_batch([x_feature, x_market], y_train_end)
         print(model.metrics_names[0] , ':' , train[0])
         print(model.metrics_names[1] , ':' , train[1])
 
 
 
-def testing(x_test_folder, y_test_foloder,model):
+def testing(x_feature_test_folder, x_market_test_folder, y_test_folder,model):
     duo_test_list = []
-    x_test_list= os.listdir(x_test_folder)
-    x_test_list=sorted(x_test_list)
+    x_feature_test_list= os.listdir(x_feature_test_folder)
+    x_feature_test_list=sorted(x_feature_test_list)
+    if x_feature_test_list[0] == '.DS_Store':
+        x_feature_test_list.pop(0)
+    x_market_test_list= os.listdir(x_market_test_folder)
+    x_market_test_list=sorted(x_market_test_list)
+    if x_market_test_list[0] == '.DS_Store':
+        x_market_test_list.pop(0)
     y_test_list=os.listdir(y_test_folder)
     y_test_list=sorted(y_test_list)
+    if y_test_list[0] == '.DS_Store':
+        y_test_list.pop(0)
     for i in range (len(y_test_list)):
-        duo=[x_test_list[i],y_test_list[i]]
+        duo=[x_feature_test_list[i], x_market_test_list[i],y_test_list[i]]
         duo_test_list.append(duo)
     duo_test_list=[duo for duo in duo_test_list if duo[0][-4:]=='.npy']
 
     num_sample = 0
     num_right_sample = 0
     for duo in duo_test_list:
-        x_test = np.load('data_backup/feed_data/x_test/'+duo[0])
-        y_test = np.load('data_backup/feed_data/y_test/'+duo[1])
-        num_sample += x_test.shape[0]
+        x_feature_test = np.load('data_backup/feed_data/x_feature_test/'+duo[0])
+        x_market_test = np.load('data_backup/feed_data/x_market_test/'+duo[1])
+        y_test = np.load('data_backup/feed_data/y_test/'+duo[2])
+        num_sample += x_feature_test.shape[0]
         y_test_list = []
         for trend in y_test:
             new_value = trend + 1
@@ -174,33 +197,38 @@ def testing(x_test_folder, y_test_foloder,model):
             y_test_list.append(code)
         y_test_end = np.asarray(y_test_list)
         print('-------test--------')
-        test = model.test_on_batch(x_test, y_test_end)
+        test = model.test_on_batch([x_feature_test, x_market_test], y_test_end)
         print(model.metrics_names[0], ':', test[0])
         print(model.metrics_names[1], ':', test[1])
-        num_right_sample += test[1] * x_test.shape[0]
+        num_right_sample += test[1] * x_feature_test.shape[0]
 
     print('total performance{}/{} = {}'.format(int(num_right_sample), num_sample, num_right_sample/num_sample))
 
 
+
+
 if __name__ == "__main__":
     model = han()
-    optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    optimizer = keras.optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     # Put your training data folder path
-    x_train_folder='data_backup/feed_data/x_train'
+    x_feature_train_folder='data_backup/feed_data/x_feature_train'
     y_train_folder='data_backup/feed_data/y_train'
-    x_test_folder = 'data_backup/feed_data/x_test'
+    x_market_train_folder = 'data_backup/feed_data/x_market_train'
+
+    x_market_test_folder = 'data_backup/feed_data/x_market_test'
+    x_feature_test_folder = 'data_backup/feed_data/x_feature_test'
     y_test_folder = 'data_backup/feed_data/y_test'
-    epochs=200
+    epochs=500
 	
-    duo_list= twin_creation(x_train_folder, y_train_folder)
+    duo_list= twin_creation(x_feature_train_folder, x_market_train_folder,y_train_folder)
     for epoch in range(epochs):
         for k,duo in enumerate(duo_list):
             print('fitting on firm nb {} out of 494 epoch {}'.format(k,epoch))
-            training(duo[0],duo[1],model)
+            training(duo[0],duo[1], duo[2],model)
         epoch += 1
 
-    testing(x_test_folder, y_test_folder, model)
+    testing(x_feature_test_folder, x_market_test_folder,y_test_folder, model)
 
     model.save('your_model_{}epochs.hdf5'.format(epochs))
     
